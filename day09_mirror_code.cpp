9c4280108fd8348349cfefbb0a35e7c1efa15794
Khushi <mac@MACs-MacBook-Pro.local>
2026-09-21 19:23:46 +0530

    Day 9 - Mirror the Code Name

---
+++ day09_mirror_code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
