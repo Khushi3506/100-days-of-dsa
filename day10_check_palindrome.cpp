d0cd5eb22ac43014e46586fe4ac82f0b6494fcbf
Khushi <mac@MACs-MacBook-Pro.local>
2026-09-21 19:27:17 +0530

    Day 10 - Check Palindrome

---
+++ day10_check_palindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int left = 0;
+    int right = s.length() - 1;
+
+    bool palindrome = true;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            palindrome = false;
+            break;
+        }
+
+        left++;
+        right--;
+    }
+
+    if (palindrome)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
